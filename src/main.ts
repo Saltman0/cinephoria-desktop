3fca4540d5a1cbdc4ee6d547a26df135c367c36d
Saltman0 <baudoin.mathieu@protonmail.com>
2025-02-14 12:40:17 +0100

    Main files updated

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./components/app/app.config";
-import { LoginComponent } from "./components/login/login.component";
+import { HeaderComponent } from "./components/header/header.component";
 
-bootstrapApplication(LoginComponent, appConfig).catch((err) =>
+bootstrapApplication(HeaderComponent, appConfig).catch((err) =>
   console.error(err)
 );
