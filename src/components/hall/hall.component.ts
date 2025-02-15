60384d92ca13332e6f9ecd4793cebd6b1327468a
Saltman0 <baudoin.mathieu@protonmail.com>
2025-02-15 19:42:02 +0100

    Hall component updated

--- src/components/hall/hall.component.ts
+++ src/components/hall/hall.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import { Component, Input } from '@angular/core';
+import {NgOptimizedImage, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-hall',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgStyle
   ],
   templateUrl: './hall.component.html',
   styleUrl: './hall.component.css'
 })
 export class HallComponent {
-
+  @Input() number: number = 777;
+  @Input() currentMovieTitle: string = "Nom du film actuel";
+  @Input() currentMovieImage: string = "Image du film actuel";
+  @Input() startHour: string = "xxhxx";
+  @Input() endHour: string = "xxhxx";
+  @Input() numberOfIncidents: number = 777;
 }
