1a1933cc86b20b7353e2ca123fc70aab99323ca9
Saltman0 <baudoin.mathieu@protonmail.com>
2025-02-18 23:51:41 +0100

    Login component updated

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angula
 import { NgOptimizedImage } from "@angular/common";
 import { ApiService } from "../../services/api/api.service";
 import { DatabaseService } from "../../services/database/database.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ export class LoginComponent {
     password: new FormControl('', [Validators.required])
   });
 
-  constructor(private apiService: ApiService, private databaseService: DatabaseService) {}
+  constructor(private router: Router, private apiService: ApiService, private databaseService: DatabaseService) {}
 
   submit() {
 
@@ -32,6 +33,7 @@ export class LoginComponent {
             this.apiService.getUser(token).subscribe(
                 response => {
                     console.log(response);
+                    this.router.navigate(['hall-list-component']);
                 }
             )
         }
