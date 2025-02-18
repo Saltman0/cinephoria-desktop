ad02347c30b0d294bcc1be0fe868b2acad3aeb25
Saltman0 <baudoin.mathieu@protonmail.com>
2025-02-18 23:52:07 +0100

    Routes added

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from "@angular/router";
+import { HallListComponent } from "../hall-list/hall-list.component";
+import { IncidentListComponent } from "../incident-list/incident-list.component";
+import { IncidentReportComponent } from "../incident-report/incident-report.component";
+import { LoginComponent } from "../login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'hall-list-component', component: HallListComponent },
+    { path: 'incident-list-component', component: IncidentListComponent },
+    { path: 'incident-report-component', component: IncidentReportComponent },
+    { path: 'login-component', component: LoginComponent }
+];
