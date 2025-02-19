2bcb084974dcea1ac2f994437e7d37bc5aeba9db
Saltman0 <baudoin.mathieu@protonmail.com>
2025-02-19 12:37:48 +0100

    Incident report component updated

--- src/components/incident-report/incident-report.component.ts
+++ src/components/incident-report/incident-report.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import { NgOptimizedImage } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-incident-report',
   standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [NgOptimizedImage],
   templateUrl: './incident-report.component.html',
   styleUrl: './incident-report.component.css'
 })
 export class IncidentReportComponent {
+  constructor(private router: Router) {}
 
+  submitIncident() {
+    this.router.navigate(['hall-list']);
+  }
+
+  returnToHallList() {
+    this.router.navigate(['hall-list']);
+  }
 }
