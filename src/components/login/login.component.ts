31b84f8b97dcce3228ea6a40e47a209e015f60ed
Saltman0 <baudoin.mathieu@protonmail.com>
2025-03-05 23:29:33 +0100

    Login component updated (WIP)

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -4,6 +4,7 @@ import { NgOptimizedImage } from "@angular/common";
 import { ApiService } from "../../services/api/api.service";
 import { DatabaseService } from "../../services/database/database.service";
 import { Router } from "@angular/router";
+import {UserFactory} from "../../factories/user.factory";
 
 @Component({
   selector: 'app-login',
@@ -14,31 +15,28 @@ import { Router } from "@angular/router";
 })
 export class LoginComponent {
   loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
+    email: new FormControl('baudoin.mathieu@protonmail.com', [Validators.required, Validators.email]),
+    password: new FormControl('0123456789', [Validators.required])
   });
 
-  constructor(private router: Router, private apiService: ApiService, private databaseService: DatabaseService) {}
+  constructor(private router: Router,
+              private apiService: ApiService,
+              private databaseService: DatabaseService,
+              private userFactory: UserFactory) {}
 
-  submit() {
+  async submit() {
 
-      this.router.navigate(['hall-list']);
+    const response = await this.apiService.login2(<string>this.loginForm.value.email, <string>this.loginForm.value.password);
 
-    /*let token;
+    console.log(response.value);
 
-    this.apiService.login(<string>this.loginForm.value.email, <string>this.loginForm.value.password).subscribe(
-        response => {
-            token = response.value;
-            console.log(token);
-            this.databaseService.loadDatabase();
-            // TODO Récupérer toutes les données
-            this.apiService.getUser(token).subscribe(
-                response => {
-                    console.log(response);
-                    this.router.navigate(['hall-list-component']);
-                }
-            )
-        }
-    );*/
+    const responseUser = await this.apiService.getUser(response.value);
+
+    console.log(responseUser);
+
+    // TODO Effectuer les différentes requêtes pour stocker dans une BDD locale
+    await this.databaseService.addUser(this.userFactory.create(responseUser.id, response.firstName, response.lastName));
+
+    //this.router.navigate(['hall-list']);
   }
 }
