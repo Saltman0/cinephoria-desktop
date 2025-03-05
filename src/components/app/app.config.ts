11ffc4e4c7950c7df31e2d2ca90f85aae1ba832b
Saltman0 <baudoin.mathieu@protonmail.com>
2025-03-05 23:28:30 +0100

    app.config.ts updated

--- src/components/app/app.config.ts
+++ src/components/app/app.config.ts
@@ -1,13 +1,24 @@
-import { ApplicationConfig } from "@angular/core";
+import {ApplicationConfig, inject} from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { provideHttpClient, withFetch } from "@angular/common/http";
+import {provideApollo} from "apollo-angular";
+import {HttpLink} from "apollo-angular/http";
+import {InMemoryCache} from "@apollo/client/core";
 
 export const appConfig: ApplicationConfig = {
   providers: [
       provideRouter(routes),
       provideHttpClient(
           withFetch()
-      )
+      ),
+      provideApollo(() => {
+          const httpLink = inject(HttpLink);
+
+          return {
+              link: httpLink.create({ uri: '/graphql' }),
+              cache: new InMemoryCache()
+          };
+      })
   ],
 };
