1ca2ed504a5e09ee7c8a836c443a907d157340fc
Saltman0 <baudoin.mathieu@protonmail.com>
2025-03-05 23:28:47 +0100

    Database service updated

--- src/services/database/database.service.ts
+++ src/services/database/database.service.ts
@@ -1,15 +1,61 @@
 import { Injectable } from '@angular/core';
-import Database from "@tauri-apps/plugin-sql";
-import {HttpClient} from "@angular/common/http";
+import Dexie, { Table } from 'dexie';
+import {JwtToken} from "../../models/jwt-token.model";
+import {User} from "../../models/user.model";
+import {Hall} from "../../models/hall.model";
+import {CurrentShowtime} from "../../models/current-showtime.model";
+import {Movie} from "../../models/movie.model";
+import {Incident} from "../../models/incident.model";
 
 @Injectable({
   providedIn: 'root'
 })
-export class DatabaseService {
+export class DatabaseService extends Dexie {
+  jwtTokens!: Table<JwtToken, number>;
+  users!: Table<User, number>;
+  halls!: Table<Hall, number>;
+  currentShowtimes!: Table<CurrentShowtime, number>;
+  movies!: Table<Movie, number>;
+  incidents!: Table<Incident, number>;
 
-  constructor(private httpClient: HttpClient) {}
+  public constructor() {
+    super('CinephoriaDatabase');
+    this.version(1).stores({
+      jwtTokens: '++id, value',
+      users: '++id, firstName, lastName',
+      halls: '++id, number, currentShowtime, movie',
+      currentShowtimes: '++id, movie, startTime, endTime',
+      movies: '++id, title, imageUrl',
+      incidents: '++id, type, description'
+    });
+    /*this.on('populate', () => this.populate());*/
+  }
 
-  async loadDatabase() {
-    await Database.load("sqlite:test.db");
+  public async deleteDatabase(): Promise<void> {
+    this.close();
+  }
+
+  public async addJwtToken(jwtToken: JwtToken): Promise<number> {
+    return this.jwtTokens.add(jwtToken);
+  }
+
+  public async addUser(user: User): Promise<number> {
+    return this.users.add(user);
+  }
+
+  public async addHall(hall: Hall): Promise<number> {
+    return this.halls.add(hall);
+  }
+
+  public async addCurrentShowtime(currentShowtime: CurrentShowtime): Promise<number> {
+    return this.currentShowtimes.add(currentShowtime);
+  }
+
+  public async addMovie(movie: Movie): Promise<number> {
+    return this.movies.add(movie);
+  }
+
+  public async addIncident(incident: Incident): Promise<number> {
+    return this.incidents.add(incident);
   }
 }
