4217660e5de565c975f4a314c0a58f4d5886acb4
Saltman0 <baudoin.mathieu@protonmail.com>
2025-03-15 20:14:52 +0100

    GetHalls Gql file added

---
+++ src/graphql/get-halls.gql.ts
@@ -0,0 +1,36 @@
+import { gql, Query } from 'apollo-angular';
+import { Injectable } from '@angular/core';
+import {Hall} from "../models/hall.model";
+
+export interface Response {
+    halls: Hall[];
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class GetHallsGQL extends Query<Response> {
+    document = gql`
+        query GetHalls($cinemaId: ID!) {
+            halls(cinemaId: $cinemaId) {
+                id
+                number
+                currentShowtime {
+                    id
+                    movie {
+                        id
+                        title
+                        imageURL
+                    }
+                    startTime
+                    endTime
+                }
+                incidents {
+                    id
+                    type
+                    description
+                }
+            }
+        }
+    `;
+}
