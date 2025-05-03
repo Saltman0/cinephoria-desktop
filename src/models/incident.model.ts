7c8773a4f282203f0af888389b1838e97b1214da
Saltman0 <baudoin.mathieu@protonmail.com>
2025-05-04 01:41:03 +0200

    :art: Incident model updated

--- src/models/incident.model.ts
+++ src/models/incident.model.ts
@@ -4,13 +4,14 @@ export class Incident {
     id: number|null;
     type: string;
     description: string;
+    date: Date;
     hall: Hall;
 
-
-    constructor(id: number|null, type: string, description: string, hall: Hall) {
+    constructor(id: number|null, type: string, description: string, date: Date, hall: Hall) {
         this.id = id;
         this.type = type;
         this.description = description;
+        this.date = date;
         this.hall = hall;
     }
 }
